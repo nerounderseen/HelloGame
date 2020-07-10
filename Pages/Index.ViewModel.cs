5d571f10eb064391c915c5be57aac45c8268c867
Leoniel D <nyl.degoro@outlook.com>
2020-07-10 19:58:07 +0800

    corrections made on line 38 & 39, terminated redundant operation, added x & y boundaries

--- Pages/Index.ViewModel.cs
+++ Pages/Index.ViewModel.cs
@@ -15,7 +15,8 @@ namespace HelloGame
         int AD = 0;
         int WS = 0;
         int yCor;
-
+        int xBound = 1024;
+        int yBound = 768;
         private Canvas2DContext _context;
         protected BECanvasComponent _canvasReference;
         protected ElementReference ashRef;
@@ -25,15 +26,17 @@ namespace HelloGame
             {
                 this._context = await this._canvasReference.CreateCanvas2DAsync();
                 await this._context.SetFillStyleAsync("cornflowerblue");
-                await this._context.FillRectAsync(0, 0, 1024, 768);
+                await this._context.FillRectAsync(0, 0, xBound, yBound);
                 await this._context.DrawImageAsync(ashRef, 0, 0, 64, 64, 0, 0, 64, 64);
             }
             else
             {
                 await Task.Delay(17);
                 await this._context.SetFillStyleAsync("cornflowerblue");
-                await this._context.FillRectAsync(0, 0, 1024, 768);
+                await this._context.FillRectAsync(0, 0, xBound, yBound);
                 await this._context.DrawImageAsync(ashRef, (currentFrame - 1) * 64, yCor, 64, 64, AD, WS, 64, 64);
+                if (currentFrame > maxFrames)
+                    currentFrame = 1;
             }
         }
 
@@ -45,33 +48,33 @@ namespace HelloGame
                 case "w":
                     yCor = 192;
                     currentFrame++;
-                    if (currentFrame > maxFrames)
-                        currentFrame = 1;
                     WS -= 5;
+                    if (WS < 0)
+                        WS = 0;
                     break;
                 //Move Down
                 case "s":
                     yCor = 0;
                     currentFrame++;
-                    if (currentFrame > maxFrames)
-                        currentFrame = 1;
                     WS += 5;
+                    if (WS > yBound - 64)
+                        WS = 706;
                     break;
                 //Move Left
                 case "a":
                     yCor = 64;
                     currentFrame++;
-                    if (currentFrame > maxFrames)
-                        currentFrame = 1;
                     AD -= 5;
+                    if (AD < 0)
+                        AD = 0;
                     break;
                 //Move Right
                 case "d":
                     yCor = 128;
                     currentFrame++;
-                    if (currentFrame > maxFrames)
-                        currentFrame = 1;
                     AD += 5;
+                    if (AD > xBound - 64)
+                        AD = 960;
                     break;
             }
         }
